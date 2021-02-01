c620f23db1e76995075e5b7be4b0d9cd53f0ebaf
egg82 <eggys82@gmail.com>
2021-02-01 13:13:52 -0700

    Add more facts APIs

--- src/main/java/me/egg82/echo/commands/FactCommand.java
+++ src/main/java/me/egg82/echo/commands/FactCommand.java
@@ -6,12 +6,15 @@ import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Syntax;
 import flexjson.JSONDeserializer;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import me.egg82.echo.config.CachedConfig;
+import me.egg82.echo.core.Pair;
 import me.egg82.echo.lang.Message;
 import me.egg82.echo.utils.WebUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -22,9 +25,14 @@ import org.jetbrains.annotations.Nullable;
 
 @CommandAlias("fact|facts|factoid|factoids|useless")
 public class FactCommand extends AbstractCommand {
-    private static final List<String> API_URLS = Arrays.asList(
-            "https://uselessfacts.jsph.pl/random.json?language=en",
-            "https://useless-facts.sameerkumar.website/api"
+    private static final List<Pair<String, String>> API_URLS = Arrays.asList(
+            new Pair<>("https://uselessfacts.jsph.pl/random.json?language=en", "text"),
+            new Pair<>("https://useless-facts.sameerkumar.website/api", "data"),
+            new Pair<>("https://catfact.ninja/fact", "fact"),
+            new Pair<>("http://numbersapi.com/random/trivia", null),
+            new Pair<>("http://numbersapi.com/random/year", null),
+            new Pair<>("http://numbersapi.com/random/date", null),
+            new Pair<>("http://numbersapi.com/random/math", null)
     );
 
     private static final Random random = new Random();
@@ -65,19 +73,23 @@ public class FactCommand extends AbstractCommand {
     }
 
     public static @NotNull CompletableFuture<String> get() {
-        return WebUtil.getUnclosedResponse(API_URLS.get(random.nextInt(API_URLS.size()))).thenApplyAsync(response -> {
+        Pair<String, String> data = API_URLS.get(random.nextInt(API_URLS.size()));
+        String url = data.getT1();
+        while (url.contains("%d")) {
+            url = String.format(url, random.nextInt(100));
+        }
+
+        return WebUtil.getUnclosedResponse(url).thenApplyAsync(response -> {
             try (response) {
+                if (data.getT2() == null) {
+                    return response.body().string();
+                }
+
                 JSONDeserializer<Map<String, Object>> modelDeserializer = new JSONDeserializer<>();
                 Map<String, Object> retVal = modelDeserializer.deserialize(response.body().charStream());
-                if (retVal == null || retVal.isEmpty()) {
-                    return null;
-                }
-                if (retVal.containsKey("text")) {
-                    return (String) retVal.get("text");
-                } else if (retVal.containsKey("data")) {
-                    return (String) retVal.get("data");
-                }
-                return null;
+                return retVal == null || retVal.isEmpty() ? null : (String) retVal.get(data.getT2());
+            } catch (IOException ex) {
+                throw new CompletionException(ex);
             }
         });
     }
